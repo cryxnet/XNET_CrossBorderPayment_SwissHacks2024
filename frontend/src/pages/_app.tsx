267ca212405cca6de94761f91c7bae590b82e913
stefanlaux <stefan.laux@edu.tbz.ch>
2024-06-29 14:56:36 +0200

    fix nav

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,8 +5,8 @@ import { Button, NextUIProvider } from '@nextui-org/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <div><Button color='primary'>Hello World</Button></div>
+    <NextUIProvider className={"h-screen"}>
+      <div className={"fixed"}><Button color='primary'>Hello World</Button></div>
       <Component {...pageProps} />
     </NextUIProvider>
   );
---
+++ frontend/src/pages/contacts.tsx
@@ -0,0 +1,8 @@
+
+export default function Contacts() {
+    return (
+       <div className={"w-full h-full flex border-1"}>
+            <p>Your Contacts</p>
+       </div>
+    );
+}
