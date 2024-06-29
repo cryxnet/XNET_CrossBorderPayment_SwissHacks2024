1719747d1dfad2edf67a86717b9a76136e2a1308
nostu <89507913+Noestu@users.noreply.github.com>
2024-06-29 15:27:06 +0200

    finished navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,29 @@
-const Navbar = () => {
+import { Link, NavbarBrand, NavbarContent, NavbarItem, Navbar as NextNavbar } from "@nextui-org/react";
+import { useRouter } from "next/router";
 
+const NavbarItems = [{ text: "Home", href: "/" }, { text: "Transactions", href: "/transactions" }, { text: "Contacts", href: "/contacts" }, { text: "Alerts", href: "/alerts" }]
+
+const Navbar = () => {
+    const router = useRouter();
+
+    return (
+        <NextNavbar position="sticky" maxWidth="full">
+            <NavbarBrand className="w-1/3">
+                <p className="font-bold text-inherit">ACME</p>
+            </NavbarBrand>
+
+            <NavbarContent  justify="center">
+                {NavbarItems.map((item) => {
+                    return (
+                        <NavbarItem className="[&:not(:last-child)]:mr-8" isActive={router.pathname === item.href}><Link href={item.href}>{item.text}</Link></NavbarItem>
+                    )
+                })}
+            </NavbarContent>
+
+            <NavbarContent justify="end" className="w-1/3">
+                <div>Profile Picture</div>
+            </NavbarContent>
+        </NextNavbar>
+    )
 }
 export default Navbar;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,12 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 // 1. import `NextUIProvider` component
 import { Button, NextUIProvider } from '@nextui-org/react';
+import Navbar from '@/components/Navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider className={"h-screen"}>
-      <div className={"fixed"}><Button color='primary'>Hello World</Button></div>
+    <NextUIProvider className="w-screen">
+      <Navbar/>
       <Component {...pageProps} />
     </NextUIProvider>
   );
