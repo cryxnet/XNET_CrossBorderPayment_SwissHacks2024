975f75ded5eea5ff1dde5f296ec0f6fd0ef1e5da
nostu <89507913+Noestu@users.noreply.github.com>
2024-06-30 00:02:20 +0200

    finished add contact

--- xnet/src/pages/transactions.tsx
+++ xnet/src/pages/transactions.tsx
@@ -1,24 +1,36 @@
 import { Inter } from "next/font/google";
-import data from "../util/mock.json"
+import data from "../util/mock.json";
 import Payment from "@/components/Payment";
-import { Card } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
 import TransactionTable from "@/components/TransactionTable";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Transactions() {
-    const user = data.user;
+  const user = data.user;
 
-
-    return (
-        <div className="flex m-8 justify-between h-[calc(100vh-128px)]">
-            <div className="w-4/6 h-full" >
-                <TransactionTable user={user}/>
-            </div>
-            <div className="w-[calc(100%-(66%+4rem))]">
-                <Card className="bg-black w-full h-3/5 mb-8" ></Card>
-                <Card className="bg-black w-full h-[calc(100%-(60%+2rem))] " ></Card>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex m-8 justify-between h-[calc(100vh-128px)]">
+      <div className="w-4/6 h-full">
+        <TransactionTable user={user} />
+      </div>
+      <div className="w-[calc(100%-(66%+4rem))]">
+        <Card className="bg-black w-full h-3/5 mb-8"></Card>
+        <Card className="w-full h-[calc(100%-(60%+2rem))]">
+          <CardHeader>Add a contact</CardHeader>
+          <CardBody className="flex justify-between items-center">
+            <p className="text-center">
+              To use the money transfer function, the recipient must already be
+              added to your contacts. To add a contact, please add one with
+              clicking here.
+            </p>
+            <Button color="primary" className="w-1/2">
+              <Link href="/contacts/create">Create Contact</Link>
+            </Button>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
 }
