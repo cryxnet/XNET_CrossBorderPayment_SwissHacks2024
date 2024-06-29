3fca978f97af5494d36039e3ed8ee7298fa4a8fd
cryxnet <75471375+cryxnet@users.noreply.github.com>
2024-06-29 12:55:43 +0200

    asd

--- src/wallets.ts
+++ src/wallets.ts
@@ -7,5 +7,5 @@ const STEF_WALLET_ADDRESS = "rBeBTBDwmvsSnAJPV4y26FJBSPaEH8Qv15"
 const STEF_WALLET_SECRET = "sEd7pjRtgXyEqRXCVLzPs3wtk4oRCyX"
 
 // Create wallets from seeds
-const bobWallet = Wallet.fromSeed(BOB_WALLET_SECRET);
-const stefWallet = Wallet.fromSeed(STEF_WALLET_SECRET);
+export const bobWallet = Wallet.fromSeed(BOB_WALLET_SECRET);
+export const stefWallet = Wallet.fromSeed(STEF_WALLET_SECRET);
