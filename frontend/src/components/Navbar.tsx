58f61244b5a8019e20316444e6781fb23826601c
stefanlaux <stefan.laux@edu.tbz.ch>
2024-06-29 16:13:03 +0200

    lol

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Link, NavbarBrand, NavbarContent, NavbarItem, Navbar as NextNavbar } from "@nextui-org/react";
+import {Link, NavbarBrand, NavbarContent, NavbarItem, Navbar as NextNavbar, Avatar} from "@nextui-org/react";
 import { useRouter } from "next/router";
+import {Image} from "@nextui-org/image";
 
 const NavbarItems = [{ text: "Home", href: "/" }, { text: "Transactions", href: "/transactions" }, { text: "Contacts", href: "/contacts" }, { text: "Alerts", href: "/alerts" }]
 
@@ -9,7 +10,7 @@ const Navbar = () => {
     return (
         <NextNavbar position="sticky" maxWidth="full">
             <NavbarBrand className="w-1/3">
-                <p className="font-bold text-inherit">ACME</p>
+                <Image width={100} src={"/logo.png"} alt={"lol"} className="font-bold text-inherit"></Image>
             </NavbarBrand>
 
             <NavbarContent justify="center">
@@ -25,7 +26,7 @@ const Navbar = () => {
             </NavbarContent>
 
             <NavbarContent justify="end" className="w-1/3">
-                <div>Profile Picture</div>
+                <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
             </NavbarContent>
         </NextNavbar>
     )
