9a8978e5f88d7f88aad4fd13147016845463d037
nostu <89507913+Noestu@users.noreply.github.com>
2024-06-29 23:01:09 +0200

    added grid for transactions

--- xnet/src/pages/transactions.tsx
+++ xnet/src/pages/transactions.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import data from "../util/mock.json"
 import TransactionTable from "@/components/TransactionList";
 import Payment from "@/components/Payment";
+import { Card } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,17 +11,12 @@ export default function Transactions() {
 
 
     return (
-        <div className="flex justify-around items-center h-[calc(100vh-96px)]">
-            <Card isBlurred className="m-4 min-h-72 w-2/5">
-                <CardBody>
-                    <TransactionTable user={user} />
-                </CardBody>
-            </Card>
-            <Card isBlurred className="m-4 h-48 w-2/5">
-                <CardBody>
-                    <Payment />
-                </CardBody>
-            </Card>
+        <div className="flex m-8 justify-between h-[calc(100vh-128px)]">
+            <Card className="bg-black w-4/6 h-full" ></Card>
+            <div className="w-[calc(100%-(66%+4rem))]">
+                <Card className="bg-black w-full h-3/5 mb-8" ></Card>
+                <Card className="bg-black w-full h-[calc(100%-(60%+2rem))] " ></Card>
+            </div>
         </div>
     );
 }
