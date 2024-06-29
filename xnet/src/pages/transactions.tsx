836ea1811ffec36df74844168ad7a6cff97725e3
nostu <89507913+Noestu@users.noreply.github.com>
2024-06-29 20:45:38 +0200

    changed color

--- xnet/src/pages/transactions.tsx
+++ xnet/src/pages/transactions.tsx
@@ -12,12 +12,12 @@ export default function Transactions() {
 
     return (
         <div className="flex justify-around items-center h-[calc(100vh-96px)]">
-            <Card isBlurred className="m-4 bg-blue-200 min-h-72 w-2/5">
+            <Card isBlurred className="m-4 min-h-72 w-2/5">
                 <CardBody>
                     <TransactionList user={user} />
                 </CardBody>
             </Card>
-            <Card isBlurred className="m-4 bg-blue-200 h-48 w-2/5">
+            <Card isBlurred className="m-4 h-48 w-2/5">
                 <CardBody>
                     <Payment />
                 </CardBody>
