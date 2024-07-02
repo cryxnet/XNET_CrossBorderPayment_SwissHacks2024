828980eebdcaeac84704e579f66fefcad3f3f533
stefanlaux <stefan.laux@edu.tbz.ch>
2024-07-02 09:54:53 +0200

    home

--- xnet/src/pages/index.tsx
+++ xnet/src/pages/index.tsx
@@ -1,5 +1,9 @@
-import { Card, CardContent, List, ListItem, ListItemText, Box, Typography } from '@mui/material';
+import { List, ListItem, ListItemText, Box, Typography, IconButton} from '@mui/material';
 import data from '@/util/mock.json';
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import TransactionTable from "@/components/TransactionTable";
+
 
 export default function Home() {
     const user = data.user;
@@ -13,6 +17,7 @@ export default function Home() {
         <Box sx={{
             width: "100%",
             padding: "15px",
+            height: "100%",
             display: "flex",
             flexDirection: "column",
             gap: "20px"
@@ -20,35 +25,50 @@ export default function Home() {
             <Typography variant="h4">Hello, {user.first_name} {user.last_name}</Typography>
 
             <Box className={"flex flex-col gap-5 h-full"}>
-                <Box className={"flex gap-5"}>
-                    <Box className={"w-full"}>
-                        <Card>
-                            <CardContent>
-                                Hello
-                            </CardContent>
+                <Box className={"flex gap-5 h-40"}>
+                    <Box className={"w-full h-full"}>
+                        <Card className={"w-full h-full"}>
+                            <CardBody sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                <Box className={"flex w-full justify-between"}>
+                                    <Typography variant="body2">Balance</Typography>
+                                    <IconButton>
+                                        <AccountBalanceWalletIcon />
+                                    </IconButton>
+                                </Box>
+                                <Typography variant="h5" sx={{ fontWeight: 'bold' }}>{user.balance} {user.preferred_currency}</Typography>
+                            </CardBody>
                         </Card>
                     </Box>
-                    <Box className={"w-full"}>
-                        <Card>
-                            <CardContent>
-                                Hello
-                            </CardContent>
+                    <Box className={"w-full h-full"}>
+                        <Card className={"w-full h-full"}>
+                            <CardBody sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                <Box className={"flex w-full justify-between"}>
+                                    <Typography variant="body2">Total fees paid</Typography>
+                                    <IconButton>
+                                        <AccountBalanceWalletIcon />
+                                    </IconButton>
+                                </Box>
+                                <Typography variant="h5" sx={{ fontWeight: 'bold' }}>{user.fees_paid} {user.preferred_currency}</Typography>
+                            </CardBody>
                         </Card>
                     </Box>
-                    <Box className={"w-full"}>
-                        <Card>
-                            <CardContent>
-                                Hello
-                            </CardContent>
+                    <Box className={"w-full h-full"}>
+                        <Card className={"w-full h-full"}>
+                            <CardBody sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                <Box className={"flex w-full justify-between"}>
+                                    <Typography variant="body2">Transactions</Typography>
+                                    <IconButton>
+                                        <AccountBalanceWalletIcon />
+                                    </IconButton>
+                                </Box>
+                                <Typography variant="h5" sx={{ fontWeight: 'bold' }}>{user.transactions.length}</Typography>
+                            </CardBody>
                         </Card>
                     </Box>
                 </Box>
-
-                <Card className={"w-full h-full"}>
-                    <CardContent>
-                        Hello
-                    </CardContent>
-                </Card>
+                <Box className={"h-full"}>
+                    <TransactionTable user={user} />
+                </Box>
             </Box>
 
         </Box>
